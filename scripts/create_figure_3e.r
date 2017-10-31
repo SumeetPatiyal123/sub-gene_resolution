6b6c47482b3056ae5cce23ec07b56490f03c0e71
SumeetPatiyal123 <33252748+SumeetPatiyal123@users.noreply.github.com>
2017-10-31 16:22:12 +0530

    Corrections in scripts for figure3

--- scripts/create_figure_3e.r
+++ scripts/create_figure_3e.r
@@ -15,54 +15,53 @@ g_legend<-function(a.gplot){
 
 #BLCA
 
-pred.data <- read.table (file = "../raw_data/matrix_blca.txt", sep = "\t", header = TRUE, check.names = FALSE)
+pred.data <- read.table (file = "matrix_blca.txt", sep = "\t", header = TRUE, check.names = FALSE)
 pred.melt <- melt (subset (pred.data, NPat > 4))
 sub.analyze <- subset (pred.melt, !(variable %in% c ("NPat", "Group", "Detected")))
 
-sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("Missense somatic drivers in matching tissue",  "Missense somatic drivers in other tissues", "Other somatic drivers in other tissues", "Other somatic drivers in matching tissue", "Other germline drivers in other tissues", "No"))
+sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("No","Other germline drivers in other tissues","Other somatic drivers in matching tissue","Other somatic drivers in other tissues", "Missense somatic drivers in other tissues", "Missense somatic drivers in matching tissue"))
 sub.analyze$variable <- factor (sub.analyze$variable, levels = c ("MutSigCV", "OncodriveFM", "Hotspot", "OncodriveCLUST", "NMC", "MutSig-CL", "iSIMPRe", "iPAC", "GraphPAC", "SpacePAC", "CLUMPS", "e-Driver", "ActiveDriver", "LowMACA", "e-Driver3D"))
 
 sub.blca.plot <- subset (sub.analyze, value < 0.05)
-blca.plot <- ggplot (sub.blca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("#FE0000", "orange","#2F496E", "#2988BC", "#5CC5EF", "white"), name = "Known driver role?") + ggtitle ("BLCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
+blca.plot<-ggplot (sub.blca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("white","#5CC5EF","#2988BC","#2F496E","orange","#FE0000"), name = "Known driver role?") + ggtitle ("BRCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
 
-blca.legend <- ggplot (sub.blca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks = element_line (color = "black"), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_text (angle = 45, hjust = 1), legend.position = "right", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("#FE0000", "orange","#2F496E", "#2988BC", "#5CC5EF", "white"), name = "Known driver role?") + ggtitle ("BLCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
+blca.legend <- ggplot (sub.blca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks = element_line (color = "black"), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_text (angle = 45, hjust = 1), legend.position = "right", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("white","#5CC5EF","#2988BC","#2F496E","orange","#FE0000"), name = "Known driver role?") + ggtitle ("BLCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
 plot.legend <- g_legend (blca.legend)
-
 #NOW BRCA
 
-pred.data <- read.table (file = "../raw_data/matrix_brca.txt", sep = "\t", header = TRUE, check.names = FALSE)
+pred.data <- read.table (file = "matrix_brca.txt", sep = "\t", header = TRUE, check.names = FALSE)
 pred.melt <- melt (subset (pred.data, NPat > 4))
 sub.analyze <- subset (pred.melt, !(variable %in% c ("NPat", "Group", "Detected")))
 
-sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("Missense somatic drivers in matching tissue",  "Missense somatic drivers in other tissues", "Other somatic drivers in other tissues", "Other somatic drivers in matching tissue", "Other germline drivers in other tissues", "No"))
+sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("No","Other germline drivers in other tissues","Other somatic drivers in matching tissue","Other somatic drivers in other tissues", "Missense somatic drivers in other tissues", "Missense somatic drivers in matching tissue"))
 sub.analyze$variable <- factor (sub.analyze$variable, levels = c ("MutSigCV", "OncodriveFM", "Hotspot", "OncodriveCLUST", "NMC", "MutSig-CL", "iSIMPRe", "iPAC", "GraphPAC", "SpacePAC", "CLUMPS", "e-Driver", "ActiveDriver", "LowMACA", "e-Driver3D"))
 
 sub.brca.plot <- subset (sub.analyze, value < 0.05)
-brca.plot <- ggplot (sub.brca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("#FE0000", "orange","#2F496E", "#2988BC", "#5CC5EF", "white"), name = "Known driver role?") + ggtitle ("BRCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
+brca.plot <- ggplot (sub.brca.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("white","#5CC5EF","#2988BC","#2F496E","orange","#FE0000"), name = "Known driver role?") + ggtitle ("BRCA") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
 
 #NOW GBM
 
-pred.data <- read.table (file = "../raw_data/matrix_gbm.txt", sep = "\t", header = TRUE, check.names = FALSE)
+pred.data <- read.table (file = "matrix_gbm.txt", sep = "\t", header = TRUE, check.names = FALSE)
 pred.melt <- melt (subset (pred.data, NPat > 4))
 sub.analyze <- subset (pred.melt, !(variable %in% c ("NPat", "Group", "Detected")))
 
-sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("Missense somatic drivers in matching tissue",  "Missense somatic drivers in other tissues", "Other somatic drivers in other tissues", "Other somatic drivers in matching tissue", "Other germline drivers in other tissues", "No"))
+sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("No","Other germline drivers in other tissues","Other somatic drivers in matching tissue","Other somatic drivers in other tissues", "Missense somatic drivers in other tissues", "Missense somatic drivers in matching tissue"))
 sub.analyze$variable <- factor (sub.analyze$variable, levels = c ("MutSigCV", "OncodriveFM", "Hotspot", "OncodriveCLUST", "NMC", "MutSig-CL", "iSIMPRe", "iPAC", "GraphPAC", "SpacePAC", "CLUMPS", "e-Driver", "ActiveDriver", "LowMACA", "e-Driver3D"))
 
 sub.gbm.plot <- subset (sub.analyze, value < 0.05)
-gbm.plot <- ggplot (sub.gbm.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("#FE0000","orange", "#2988BC", "white"), name = "Known driver role?")  + ggtitle ("GBM") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
+gbm.plot <- ggplot (sub.gbm.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks.y = element_line (color = "black"), axis.ticks.x = element_blank(), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("white","#5CC5EF","#2988BC","orange","#FE0000","#2F496E"), name = "Known driver role?")  + ggtitle ("GBM") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
 
 #NOW LUAD
 
-pred.data <- read.table (file = "../raw_data/matrix_luad.txt", sep = "\t", header = TRUE, check.names = FALSE)
+pred.data <- read.table (file = "matrix_luad.txt", sep = "\t", header = TRUE, check.names = FALSE)
 pred.melt <- melt (subset (pred.data, NPat > 4))
 sub.analyze <- subset (pred.melt, !(variable %in% c ("NPat", "Group", "Detected")))
 
-sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("Missense somatic drivers in matching tissue",  "Missense somatic drivers in other tissues", "Other somatic drivers in other tissues", "Other somatic drivers in matching tissue", "Other germline drivers in other tissues", "No"))
+sub.analyze$AllDriver <- factor (sub.analyze$AllDriver, levels = c ("No","Other germline drivers in other tissues","Other somatic drivers in matching tissue","Other somatic drivers in other tissues", "Missense somatic drivers in other tissues", "Missense somatic drivers in matching tissue"))
 sub.analyze$variable <- factor (sub.analyze$variable, levels = c ("MutSigCV", "OncodriveFM", "Hotspot", "OncodriveCLUST", "NMC", "MutSig-CL", "iSIMPRe", "iPAC", "GraphPAC", "SpacePAC", "CLUMPS", "e-Driver", "ActiveDriver", "LowMACA", "e-Driver3D"))
 
 sub.luad.plot <- subset (sub.analyze, value < 0.05)
-luad.plot <- ggplot (sub.luad.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks = element_line (color = "black"), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_text (angle = 45, hjust = 1), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("#FE0000", "orange", "#2F496E", "#5CC5EF", "white"), name = "Known driver role?") + ggtitle ("LUAD") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
+luad.plot <- ggplot (sub.luad.plot, aes (x = variable)) + geom_bar (position = "fill", aes (fill = AllDriver), color = "black") + theme (axis.text = element_text (color = "black"), axis.ticks = element_line (color = "black"), axis.line.x = element_line (color = "black"), axis.line.y = element_line (color = "black"), panel.background = element_blank(), axis.text.x = element_text (angle = 45, hjust = 1), legend.position = "none", axis.title = element_blank()) + scale_y_continuous (labels = scales::percent) + scale_fill_manual (values = c ("white","#5CC5EF","#2988BC","orange","#FE0000","#2F496E"), name = "Known driver role?") + ggtitle ("LUAD") + theme (plot.title = element_text (hjust = 0.5, size = 12, face = "italic"))
 
 #PUT IT TOGETHER
 
